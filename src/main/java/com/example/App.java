5bd31495b9f4bb8d77d18c401892bdd8a6fd81c8
nandini200310 <nandinir.22.becs@acharya.ac.in>
2025-05-22 15:27:41 +0530

    first commit:myapp

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(" gangu ranjan lopper");
+        int a=5;
+        int b=4;
+        System.out.println("there for their addition of 5 and 4 is : Ranjan lupper which is" +(a+b));
+    }
+}
